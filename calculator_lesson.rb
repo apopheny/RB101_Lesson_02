992618a2b3d2c0c2e02b8effb1b5ac965eb29916
Chris Douglass <cedouglass@gmail.com>
2021-06-12 18:54:55 -0400

    partway through calculator stretch goals

--- calculator_lesson.rb
+++ calculator_lesson.rb
@@ -1,9 +1,46 @@
+# Better integer validation.
+
+#   The current method of validating the input for a number is very weak. It's also not fully accurate, as you cannot enter a 0. Come up with a better way of validating input for integers.
+
+      # def valid_number?(num)
+      #   if num.start_with?("0")
+      #     true
+      #   else
+      #     (num.to_i != 0) && (num.to_i / num.to_i == 1)
+      #   end
+      # end
+
+# Number validation.
+
+#   Suppose we're building a scientific calculator, and we now need to account for inputs that include decimals. How can we build a validating method, called number?, to verify that only valid numbers -- integers or floats -- are entered?
+
+      # def valid_number?(num)
+      #   if num.start_with?("0") || num.start_with?(".")
+      #     true
+      #   else
+      #     (num.to_i != 0) && (num.to_i / num.to_i == 1)
+      #   end
+      # end
+
+# Our operation_to_message method is a little dangerous, because we're relying on the case statement being the last expression in the method. What if we needed to add some code after the case statement within the method? What changes would be needed to keep the method working with the rest of the program?
+
+# Extracting messages in the program to a configuration file.
+
+#   There are lots of messages sprinkled throughout the program. Could we move them into some configuration file and access by key? This would allow us to manage the messages much easier, and we could even internationalize the messages.
+
+# Your calculator program is a hit, and it's being used all over the world! Problem is, not everyone speaks English. You need to now internationalize the messages in your calculator. You've already done the hard work of extracting all the messages to a configuration file. Now, all you have to do is send that configuration file to translators and call the right translation in your code.
+require "pry"
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def valid_number?(num)
-  num.to_i != 0
+  if num.start_with?("0") || num.start_with?(".")
+    true
+  else
+    (num.to_i != 0) && (num.to_i / num.to_i == 1)
+  end
 end
 
 def operation_to_message(op)
@@ -82,16 +119,19 @@ loop do # main loop
 
   result =  case operator
             when '1'
-              number1.to_i() + number2.to_i()
+              number1.to_f() + number2.to_f()
             when '2'
-              number1.to_i() - number2.to_i()
+              number1.to_f() - number2.to_f()
             when '3'
-              number1.to_i() * number2.to_i()
+              number1.to_f() * number2.to_f()
             when '4'
               number1.to_f() / number2.to_f()
             end
+  
 
-  prompt("The result is #{result}")
+  unless result == nil
+    prompt("The result is #{result}")
+  end
 
   prompt("Do you want to perform another calculation? (Y to calculate again)")
   answer = Kernel.gets().chomp()
@@ -99,3 +139,5 @@ loop do # main loop
 end
 
 prompt("Thank you for using the calculator. Goodbye.")
+
+
